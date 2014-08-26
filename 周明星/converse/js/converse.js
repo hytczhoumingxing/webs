225a6a443d2f3f5969fbf2cfb76c87dc740c5ece
hytczhoumingxing <1027883520@qq.com>
2014-08-26 21:15:35 +0800

    2014-8-26
    开始QQ邮箱

--- 周明星/converse/js/converse.js
+++ 周明星/converse/js/converse.js
@@ -1,35 +1,94 @@
 $(function(){
 	$(".maina").click(function(){
 
-		$(this).parent().animate(
+		$(this).parent().stop(true,false).animate(
 		{
 			"top":"-845px",
 		}
 			,1000);
-		$(this).parent().next().animate(
+		$(this).parent().next().stop(true,false).animate(
 		{
 			"top":"-845px",
 		}
 			,1000);
-		$(this).parent().next().find(".same").animate({
-
-               "width":"105%",
-               "hight":"105%",
-               "margin-left":"-15px",
-    	        "margin-top":"5px",
-		},2000);
+		
+		$(this).parent().next().find(".same").css({
+			"transform":"scale(1.03)"
+		});
+		
 	});
 	$(".up").click(function(){
 
-		$(this).parent().animate(
+		$(this).parent().stop(true,false).animate(
 		{
 			"top":"0px",
 		}
 			,1000);
-		$(this).parent().prev().animate(
+		$(this).parent().prev().stop(true,false).animate(
 		{
 			"top":"0px",
 		}
 			,1000);
 	});
+	$(".m2lia").hover(function(){
+         $(this).find(".same").attr("src","css/images/b2.png");
+	},function(){
+		$(this).find(".same").attr("src","css/images/b1.png");
+	});
+	$(".m2li1a").hover(function(){
+         $(this).find(".same").attr("src","css/images/c2.png");
+	},function(){
+		$(this).find(".same").attr("src","css/images/c1.png");
+	});
+	$(".m2li2a").hover(function(){
+         $(this).find(".same").attr("src","css/images/d2.png");
+	},function(){
+		$(this).find(".same").attr("src","css/images/d1.png");
+	});
+
+	// $(window).scroll(function(){
+	// 	var scrolls = $(this).scrollTop();
+	// 	alert(scrolls);
+	// });
+	
+	// 绑定方式 
+    $('.main2').bind('mousewheel', function(event, delta) {  
+	    if(delta>0)
+	    {
+	    	
+	    	$(".up").trigger("click"); 
+	    }
+	});  
+	$('.main1').bind('mousewheel', function(event, delta) {  
+	     if(delta<0)
+	    {
+	    	$(".maina").trigger("click"); 
+	    	
+	    } 
+	}); 
+	  
+	// // 事件函数方式  
+	// $('.main2').mousewheel(function(event, delta) {  
+	//     alert(delta);
+	// });  
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
